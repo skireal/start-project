e501f8e9e6b0499ff6a6a58cd47ebe3ed325b633
Kirill Shishov <skireal@mail.ru>
2021-03-04 23:58:03 +0300

    Добавил таск лив релоада

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp')
 
+const server = require('./gulp/tasks/server')
 const pug2html = require('./gulp/tasks/pug2html')
 const styles = require('./gulp/tasks/styles')
 const prettifyHtml = require('./gulp/tasks/prettifyHtml')
+const imageMinify = require('./gulp/tasks/imageMinify')
+const clean = require('./gulp/tasks/clean')
 
-module.exports.start =  gulp.series(pug2html, styles, prettifyHtml)
+module.exports.start =  gulp.series(pug2html, styles, prettifyHtml, imageMinify, server)
