b8a3d572c67e3a03da8e77c6d389361bf3ef77d9
Kirill Shishov <skireal@mail.ru>
2021-03-04 23:49:49 +0300

    Добавил таск для минификации изображений

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,11 +1,9 @@
 const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const sass = require('gulp-sass')
-const cleanCSS = require('gulp-clean-css')
 const shorthand = require('gulp-shorthand')
 const autoprefixer = require('gulp-autoprefixer')
 const csscomb = require('gulp-csscomb')
-const rename = require("gulp-rename")
 
 module.exports = function styles() {
   return gulp.src('src/scss/*.scss')
@@ -16,12 +14,5 @@ module.exports = function styles() {
     }))
     .pipe(shorthand())
     .pipe(csscomb())
-    // .pipe(cleanCSS({
-    //   debug: true,
-    //   compatibility: '*'
-    // }, details => {
-    //   console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
-    // }))
-    // .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('build/css'))
 }
