cde63225a51615b398e96b99dd74db6def3b449c
Kirill Shishov <skireal@mail.ru>
2021-06-02 01:47:50 +0300

    Ну несколько же может быть гармошек

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -1,29 +1,35 @@
 /* Аккордеон
 ===========================================================*/
 
-let accordionTitle = document.getElementsByClassName('accordion__title');
-let accordionContentList = document.getElementsByClassName('accordion__content');
+let accordions = document.querySelectorAll('.accordion');
 
-for (var i = 0; i < accordionTitle.length; i++) {
-    accordionTitle[i].addEventListener("click", function() {
+accordions.forEach(function(element, index) {
+    var accordionTitle = element.getElementsByClassName('accordion__title');
+    var accordionContentList = element.getElementsByClassName('accordion__content');
 
-        for (var i = 0; i < accordionTitle.length; i++) {
-            if (accordionTitle[i] != this) {
-                accordionTitle[i].classList.remove('accordion__title--active');
+    for (var i = 0; i < accordionTitle.length; i++) {
+        accordionTitle[i].addEventListener("click", function() {
+            for (var i = 0; i < accordionTitle.length; i++) {
+                if (accordionTitle[i] != this) {
+                    accordionTitle[i].classList.remove('accordion__title--active');
+                }
+
+                this.classList.toggle('accordion__title--active');
             }
-            this.classList.toggle('accordion__title--active');
-        }
 
-        let accordionContent = this.nextElementSibling;
-        for (var i = 0; i < accordionContentList.length; i++) {
-            if (accordionContentList[i].style.maxHeight && accordionContentList[i] != this.nextElementSibling) {
-                accordionContentList[i].style.maxHeight = null;
+            var accordionContent = this.nextElementSibling;
+
+            for (var i = 0; i < accordionContentList.length; i++) {
+                if (accordionContentList[i].style.maxHeight && accordionContentList[i] != this.nextElementSibling) {
+                    accordionContentList[i].style.maxHeight = null;
+                }
             }
-        }
-        if (accordionContent.style.maxHeight) {
-            accordionContent.style.maxHeight = null;
-        } else {
-            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-        }
-    });
-}
+
+            if (accordionContent.style.maxHeight) {
+                accordionContent.style.maxHeight = null;
+            } else {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+            }
+        });
+    }
+});
