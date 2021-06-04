a6dba9f423c8cab3125b2d37ba8b056e375a742b
Kirill Shishov <skireal@mail.ru>
2021-06-04 13:41:41 +0300

    Update smooth-scroll-link.js

--- src/blocks/smooth-scroll-link/smooth-scroll-link.js
+++ src/blocks/smooth-scroll-link/smooth-scroll-link.js
@@ -2,30 +2,29 @@
 ===========================================================*/
 
 let smoothScrollLinks = document.querySelectorAll('.smooth-scroll-link');
+let goToBlockOffset;
 
 if (smoothScrollLinks.length > 0) {
-	smoothScrollLinks.forEach(smoothScrollLink => {
-		smoothScrollLink.addEventListener('click', onSmoothScrollLinkClick);
-	});
+    smoothScrollLinks.forEach(smoothScrollLink => {
+        smoothScrollLink.addEventListener('click', onSmoothScrollLinkClick);
+    });
 
-	function onSmoothScrollLinkClick(e) {
-		let smoothScrollLink = e.target;
-		if (smoothScrollLink.dataset.goto && document.querySelector(smoothScrollLink.dataset.goto)) {
-			let goToBlock = document.querySelector(smoothScrollLink.dataset.goto);
-			if(smoothScrollLink.closest('.smooth-scroll-link__wrapper')) {
-				let goToBlockOffset = goToBlock.getBoundingClientRect().top + pageYOffset - smoothScrollLink.closest('.smooth-scroll-link__wrapper').offsetHeight;
-			}
+    function onSmoothScrollLinkClick(e) {
+        let smoothScrollLink = e.target;
+        if (smoothScrollLink.dataset.goto && document.querySelector(smoothScrollLink.dataset.goto)) {
+            let goToBlock = document.querySelector(smoothScrollLink.dataset.goto);
+            console.log(smoothScrollLink.closest('.smooth-scroll-link__wrapper') != null);
+            if (smoothScrollLink.closest('.smooth-scroll-link__wrapper') != null) {
+                goToBlockOffset = goToBlock.getBoundingClientRect().top + pageYOffset - smoothScrollLink.closest('.smooth-scroll-link__wrapper').offsetHeight;
+            } else {
+                goToBlockOffset = goToBlock.getBoundingClientRect().top + pageYOffset - 10;
+            }
 
-			else {
-				let goToBlockOffset = goToBlock.getBoundingClientRect().top + pageYOffset - 10;
-			}
-			
-
-			window.scrollTo({
-				top: goToBlockOffset,
-				behavior: 'smooth'
-			})
-			e.preventDefault();
-		}
-	}
+            window.scrollTo({
+                top: goToBlockOffset,
+                behavior: 'smooth'
+            })
+            e.preventDefault();
+        }
+    }
 }
