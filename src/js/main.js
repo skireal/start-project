af8bd76779aad393c0a444c04a7c30c18e631f3a
Kirill Shishov <skireal@mail.ru>
2022-01-10 19:51:31 +0300

    Добавил другой вариант плавного скролла до якоря

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,42 @@
- 'use strict'
+'use strict';
 
- const getScrollSize = function() {
-     const outer = document.createElement('div');
-     const inner = document.createElement('div');
-     outer.style.overflow = 'scroll';
-     outer.classList.add('scrollbar');
-     document.body.appendChild(outer);
-     outer.appendChild(inner);
-     const scrollbarSize = outer.offsetWidth - inner.offsetWidth;
-     document.body.removeChild(outer);
-     return scrollbarSize;
- }
+const getScrollSize = function () {
+  const outer = document.createElement('div');
+  const inner = document.createElement('div');
+  outer.style.overflow = 'scroll';
+  outer.classList.add('scrollbar');
+  document.body.appendChild(outer);
+  outer.appendChild(inner);
+  const scrollbarSize = outer.offsetWidth - inner.offsetWidth;
+  document.body.removeChild(outer);
+  return scrollbarSize;
+};
 
-document.addEventListener("DOMContentLoaded", function(event) {
-   console.log('Проклятый старый DOM построен!');
-   // Добавление кастомного свойства с системной шириной скролла
-   document.documentElement.style.setProperty('--css-scroll-size', `${getScrollSize()}px`);
+document.addEventListener('DOMContentLoaded', function (event) {
+  console.log('Проклятый старый DOM построен!');
+  // Добавление кастомного свойства с системной шириной скролла
+  document.documentElement.style.setProperty(
+    '--css-scroll-size',
+    `${getScrollSize()}px`
+  );
 
+  // Плавный скролл до якоря
 
-     // window.addEventListener('resize', function(event) {
-     //     ...
-     // }, true);
- });
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
- // window.onload = function() {
- //     alert('Страница со всеми стилями и другими ресурсами загружена');
- // };
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+
+  // window.addEventListener('resize', function(event) {
+  //     ...
+  // }, true);
+});
+
+// window.onload = function() {
+//     alert('Страница со всеми стилями и другими ресурсами загружена');
+// };
